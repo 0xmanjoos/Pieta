4170d28965d978d2ec9ec7b53802b8d7aea95af9
Jotaro Kujo <0xmanjoos@protonmail.com>
2021-04-10 12:39:25 -0700

    fixed install

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-echo -e '\e[5mInstalling..\e[0m' && apt-get update && apt upgrade -y && apt install gcc && apt install g++ && apt install g++-mingw-w64-i686 && pip3 install -r requirements.txt && chmod +x /opt/pieta
-echo "Run Pieta to get started"
+echo -e '\e[5mInstalling..\e[0m' && apt update && apt upgrade -y && apt install gcc && apt install g++ && apt install g++-mingw-w64-i686 && pip3 install -r requirements.txt && cp pieta.py /opt && chmod +x /opt/pieta.py && ln -s /opt/pieta.py /usr/bin/pieta
+echo "Run pieta to get started"
